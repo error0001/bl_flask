1c9ddaa671e62f29385db9b60ba87eae59087945
error0001 <error00010001@gmail.com>
2019-04-18 11:18:58 +0300

    add addpost< and other

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask , render_template
-
+from flask import Flask , render_template, request
+from flask_sqlalchemy import  SQLAlchemy
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/andrey/PycharmProjects/bl_fls/blog.db'
+
+db = SQLAlchemy(app)
+
+
+class Blogpost(db.Model):
+    # see the guide with flask sqlalchemy
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
 
 @app.route('/')
 def index():
@@ -20,6 +35,26 @@ def post():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+@app.route('/addpost')
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    return '<h1>Title: {} Subtitle: {} Author: {} Content: {}</h1>'.format(
+        title,
+        subtitle,
+        author,
+        content
+    )
 
 
 if __name__ == '__main__':
