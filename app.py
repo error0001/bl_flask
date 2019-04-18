7e80535e0a33fbc04de5cf790dd8a2c6d31573d6
error0001 <error00010001@gmail.com>
2019-04-18 13:50:13 +0300

    local files is over, already for docker

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask , render_template, request
-from flask_sqlalchemy import  SQLAlchemy
+from flask import Flask , render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/andrey/PycharmProjects/bl_fls/blog.db'
@@ -19,7 +20,10 @@ class Blogpost(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # realization auto column
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/about')
@@ -27,9 +31,11 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Blogpost.query.filter_by(id=post_id).one()
+
+    return render_template('post.html', post=post)
 
 
 @app.route('/contact')
@@ -42,19 +48,29 @@ def add():
     return render_template('add.html')
 
 
-@app.route('/addpost')
+@app.route('/addpost', methods=['POST'])
 def addpost():
     title = request.form['title']
     subtitle = request.form['subtitle']
     author = request.form['author']
     content = request.form['content']
 
-    return '<h1>Title: {} Subtitle: {} Author: {} Content: {}</h1>'.format(
-        title,
-        subtitle,
-        author,
-        content
-    )
+    post = Blogpost(title=title,
+                    subtitle=subtitle,
+                    author=author,
+                    content=content,
+                    date_posted=datetime.now())
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+    # return '<h1>Title: {} Subtitle: {} Author: {} Content: {}</h1>'.format(
+    #     title,
+    #     subtitle,
+    #     author,
+    #     content
+    # )
 
 
 if __name__ == '__main__':
